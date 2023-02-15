674e847710524e5e1de0f36d0f8f5294f15976eb
Mohamed <aminito432@gmail.com>
2023-02-15 15:00:35 +0300

    Solved

--- control_flow.rb
+++ control_flow.rb
@@ -1,54 +1,47 @@
 def admin_login(username, password)
-  # your code hereif (username === "admin" || username === "ADMIN") && password === "12345"
-     "Access granted";
-   else 
-     "Access denied";
+  if username.downcase == 'admin' && password == '12345'
+    'Access granted'
+  else
+    'Access denied'
   end
 end
 
-
-
-
 def hows_the_weather(temperature)
-  # your code here
-
-  response = if temperature < 40
-    "brisk";
-    elsif temperature >= 40 && temperature <= 65
-    "a little chilly";
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
   elsif temperature > 85
-    "too dang hot";
-  else 
-    "perfect";
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
   end
-
-  "It's #{response} out there!";
-
 end
 
 def fizzbuzz(num)
-  # your code here
-  fuzz = if (num % 3 === 0 && num % 5 === 0) 
-    "FizzBuzz";
-   elsif (num % 3 === 0) 
-    "Fizz";
-   elsif (num % 5 === 0) 
-    "Buzz";
-   else 
-    num;
-   end
-   fuzz
-end
-
-def calculator(operation, num1, num2)
-  # your code here
-  case operation
-  when '+' then num1 + num2
-  when '-' then num1 - num2
-  when '*' then num1 * num2
-  when '/' then num1 / num2
-  else puts "Invalid operation!"
-    return nil
+  if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num
   end
 end
 
+def calculator(operation, num1, num2)
+  case operation
+  when '+'
+    num1 + num2
+  when '-'
+    num1 - num2
+  when '*'
+    num1 * num2
+  when '/'
+    num1 / num2
+  else
+    puts 'Invalid operation!'
+    nil
+  end
+end
